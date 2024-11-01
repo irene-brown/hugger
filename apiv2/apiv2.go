0b687367a4f3e6f361e98a3f8c45ab48e9aaff8d
irene-brown <irene-brown95@proton.me>
2024-11-01 09:28:44 +0000

    Update apiv2.go

--- apiv2/apiv2.go
+++ apiv2/apiv2.go
@@ -10,6 +10,44 @@ import (
 	progressbar "github.com/schollz/progressbar/v3"
 )
 
+// parse description, drop all unneeded characters
+// also add a little bit of markup
+func prepareDescription( description string ) string {
+
+	description = strings.Replace( strings.Replace( description, "\n\n", "\n", -1 ), "\n\t", "\n", -1 )
+	description = strings.Replace( description, "\t", "", -1 )
+	description = strings.Replace( description, "  ", " ", -1 )
+	description = strings.Replace( description, "\r\n", "", -1 )
+	description = strings.Replace( description, "\r", "", -1 )
+	description = strings.Trim( description, " " )
+	description = strings.Trim( description, "\n" )
+	description = strings.Trim( description, "\t" )
+	for {
+		if strings.Contains( description, "\n\n\n" ) {
+			description = strings.Replace( description, "\n\n\n", "\n\n", -1 )
+		} else {
+			break
+		}
+	}
+	
+	res := ""
+	tmpRes := strings.Split( description, "\n\n" )
+	for i, r := range tmpRes {
+		if i % 2 == 0 {
+			res += "\033[1;1m" + "### " + r + "\n"
+		} else {
+			tmp := strings.Split(r, "\n")
+			for j, _ := range tmp {
+				tmp[j] = "\t" + tmp[j]
+			}
+			r = strings.Join( tmp, "\n" )
+			res += r + "\n\n"
+		}
+	}
+	res = strings.Trim( res, "\n" )
+
+	return res
+}
 
 func ServeRequest( reqType, repoName, repoType, token, action string, files []string, private bool ) error {
 
@@ -17,18 +55,24 @@ func ServeRequest( reqType, repoName, repoType, token, action string, files []st
 	
 	switch reqType {
 	case "meta":
-		//fmt.Println("getting metadata...")
 		meta, err := client.GetMetadata( repoType, repoName )
 		if err != nil {
 			return err
 		}
-		//fmt.Println("collected metadata...")
-		description := strings.Replace( strings.Replace( meta.Description, "\n\n", "\n", -1 ), "\n\t\n", "\n", -1 )
+
+		description := prepareDescription( meta.Description )
+
 
 		tw := table.NewWriter()
+
+		reset := "\x1b[39m"
+
 		tw.AppendHeader( table.Row{ meta.Type[3:] + " " + meta.Name + " by " + meta.Creator["name"] } )
-		tw.AppendRow( table.Row{ "URL", meta.URL } )
-		tw.AppendRow( table.Row{ "License", meta.License } )
+		tw.AppendRow( table.Row{ "Name", "\033[38;2;150;200;200;1m" + meta.Name + reset } )
+		tw.AppendRow( table.Row{ "Type", "\033[38;2;100;200;200;1m" + meta.Type[3:] + reset } )
+		tw.AppendRow( table.Row{ "Author","\033[38;2;50;200;200;1m" + meta.Creator["name"] + reset } )
+		tw.AppendRow( table.Row{ "URL", "\033[38;2;0;200;200;1m" + meta.URL + reset} )
+		tw.AppendRow( table.Row{ "License", "\033[38;2;0;150;200;1m" + meta.License + reset } )
 		tw.AppendRow( table.Row{ "Description", description } )
 		
 		kw := ""
@@ -48,7 +92,12 @@ func ServeRequest( reqType, repoName, repoType, token, action string, files []st
 
 	case "download":
 		bar := setupProgressBar( int(len(files)), "Downloading..." )
-		for _, file := range files {
+		totalSteps := len(files)
+
+		for i, file := range files {
+			
+			color := getGradientColor( float64(i+1) / float64(totalSteps) )
+
 			data, err := client.DownloadFile( repoType, repoName, file )
 			if err != nil {
 				return err
@@ -57,13 +106,20 @@ func ServeRequest( reqType, repoName, repoType, token, action string, files []st
 					return err
 				}
 			}
+
+			bar.Describe( fmt.Sprintf("%s Downloading... [reset]", color) )
 			bar.Add(1)
 		}
 		fmt.Println()
+
 	case "upload":
 		bar := setupProgressBar( int(len(files)), "Uploading..." )
+		totalSteps := len(files)
 
-		for _, file := range files {
+		for i, file := range files {
+			
+			color := getGradientColor( float64(i+1) / float64(totalSteps) )
+			
 			contents, err := os.ReadFile( file )
 			if err != nil {
 				return err
@@ -72,6 +128,8 @@ func ServeRequest( reqType, repoName, repoType, token, action string, files []st
 			if err := client.UploadFile( repoType, repoName, file, contents ); err != nil {
 				return err
 			}
+			
+			bar.Describe( fmt.Sprintf("%s Uploading... [reset]", color) )
 			bar.Add(1)
 		}
 		fmt.Println()
@@ -112,10 +170,23 @@ func ServeRequest( reqType, repoName, repoType, token, action string, files []st
 			
 			tw := table.NewWriter()
 			tw.AppendHeader( table.Row{"Directory listing of " + file} )
+
+			directoriesCount := 0
+			filesCount := 0
+
 			for _, file := range repoFiles {
+				if file[len(file)-1] == '/' { // directory
+					file = "\033[38;2;0;200;200;1m" + file 
+					directoriesCount++
+				} else {
+					filesCount++
+				}
 				tw.AppendRow( table.Row{ file } )
 			}
+			tw.AppendFooter( table.Row{"Folders:", fmt.Sprintf("%d", directoriesCount) } )
+			tw.AppendFooter( table.Row{"Files:", fmt.Sprintf("%d", filesCount) } )
 			tw.AppendFooter( table.Row{"Total", fmt.Sprintf("%d", len(repoFiles))} )
+
 			tw.SetStyle( table.StyleColoredDark )
 			tw.Style().Color.Header = text.Colors{ text.BgCyan, text.FgWhite, text.Bold }
 			tw.Style().Color.Footer = text.Colors{ text.BgCyan, text.FgWhite, text.Bold }
@@ -138,12 +209,21 @@ func setupProgressBar( x int, msg string ) *progressbar.ProgressBar {
 			fmt.Sprintf("[cyan] %s [reset]", msg ),
 		),
 		progressbar.OptionSetTheme( progressbar.Theme{
-			Saucer:	"[green]=[reset]",
-			SaucerHead: "[yellow]>[reset]",
+			Saucer:	"[green]-[reset]",
+			SaucerHead: "[yellow][bold]>[reset]",
 			SaucerPadding: " ",
 			BarStart: "[",
 			BarEnd: "]",
 		} ),
+		progressbar.OptionShowCount(),
+		progressbar.OptionSetWidth( 80 ),
 	)
 	return bar
+}
+
+// return gradient for progressbar
+func getGradientColor( progress float64 ) string {
+	r := int( 255 * (1 - progress) )
+	g := int( 255 * progress )
+	return fmt.Sprintf("\033[38;2;%d;%d;0m", r, g)
 }
